60832fa7643fdfd1fa2ed1814a817d00448d5d77
Padunk <abraham.agel@gmail.com>
2021-10-11 17:24:18 +0700

    adding component to page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "@fontsource/montserrat";
+import "@fontsource/italianno";
+import "@fontsource/aleo";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../styles";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ChakraProvider theme={theme}>
+            <QueryClientProvider client={new QueryClient()}>
+                <Component {...pageProps} />
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </ChakraProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
